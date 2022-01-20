0765dae12819f19cbf639593325b1ad0e5102854
JCIII <jcostello711@gmail.com>
2022-01-20 16:33:23 -0500

    app prompts user for password length and stores the value

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,20 @@
 // Assignment code here
+function generatePassword() {
+  //Prompt user for desired length of password, specifying the range of values accetpable
+  var passwordLength = "";
+  var lengthFlag = true;
+  //Input validation loop
+  while (passwordLength == "" || lengthFlag) {
+    passwordLength = prompt("Please enter a password length. Length must be at least 8 but no more than 128.","8");
+    if (!(parseInt(passwordLength) >= 8 && parseInt(passwordLength) <= 128)) {
+      alert("Invalid entry. Please reread the instructions and try again.");
+      continue;
+    }
+    lengthFlag = false;
+  }
 
+  
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
